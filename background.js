18ef905ef6bb62cd70d829e086efb52c17fd1588
Ankit Thosar <51828020+CracDoc@users.noreply.github.com>
2025-02-15 21:06:11 +0530

    Testing COmmit

---
+++ background.js
@@ -0,0 +1,29 @@
+let countdownTimer;
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === "startCountdown") {
+        let timeLeft = message.time;
+
+        clearInterval(countdownTimer);
+        updateBadge(timeLeft);
+
+        countdownTimer = setInterval(() => {
+            timeLeft--;
+            if (timeLeft > 0) {
+                updateBadge(timeLeft);
+            } else {
+                clearInterval(countdownTimer);
+            }
+        }, 1000);
+    }
+
+    if (message.action === "clearBadge") {
+        clearInterval(countdownTimer);
+        chrome.action.setBadgeText({ text: "" });
+    }
+});
+
+function updateBadge(time) {
+    chrome.action.setBadgeText({ text: String(time) });
+    chrome.action.setBadgeBackgroundColor({ color: "#FF0000" });
+}
